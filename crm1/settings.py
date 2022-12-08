6f2e0c527cee6387bf77679468a3b6d34e27bcbc
hawa <ragab5434@gmail.com>
2022-12-08 13:20:39 +0300

    added logo and static files

--- crm1/settings.py
+++ crm1/settings.py
@@ -121,6 +121,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    Path.joinpath(BASE_DIR, 'static')
+]
+
+MEDIA_URL = 'images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
